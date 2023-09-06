0e4a9c2b381ef03b48c6f99fb582dfc310d0fe34
kimhyeon1 <rlagus814@gmail.com>
2023-09-06 14:42:01 +0900

    name 바꿨습니다

---
+++ src/main/java/com/dto/BranchDAO.java
@@ -0,0 +1,17 @@
+package com.dto;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.mybatis.SqlMapConfig;
+
+public class BranchDAO {
+	SqlSessionFactory factory = SqlMapConfig.getFactory();
+	SqlSession sqlSession;
+	
+	public BranchDAO() {
+		sqlSession = (factory).openSession(true);
+		
+	
+	}
+}
