fe1d939122dc847466bb88c767b3cfbec4522925
jeonbyungho <byho.jeon@gmail.com>
2023-09-06 23:41:24 +0900

    로그인 요청 메세지 수정 - Conten-type : form urlencoded 형태로 알맞게 변형

--- src/main/java/com/web/action/MemberLoginAction.java
+++ src/main/java/com/web/action/MemberLoginAction.java
@@ -1,16 +1,13 @@
 package com.web.action;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.tomcat.util.json.JSONParser;
-import org.apache.tomcat.util.json.ParseException;
+import org.json.simple.JSONObject;
 
 import com.dto.MemberDAO;
 import com.dto.MemberDTO;
@@ -21,44 +18,33 @@ public class MemberLoginAction extends ExcuteAction{
 		resp.setContentType("application/x-json;charset=utf-8");
 		
 		// 요청 body 읽기
-		BufferedReader reader = req.getReader();
-		StringBuilder sb = new StringBuilder();
-	    String line;
-	    while ((line = reader.readLine()) != null) {
-	        sb.append(line);
-	    }
-	    String reqBodyString = sb.toString();
+	    String userid = req.getParameter("userid");
+	    String password = req.getParameter("password");
 	    
-	    JSONParser json = new JSONParser(reqBodyString);
-	    String userid = null;
-	    String password = null;
-	    try {
-	    	Map<String, Object> map =json.parseObject();
-	    	userid = (String) map.get("userid");
-	    	password = (String) map.get("password");
-		} catch (ParseException e) {
-			
-			e.printStackTrace();
-		}
+	    System.out.println("유저 ID : " + userid);
+	    System.out.println("비밀번호 : " + password);
 	    
+	    // 요청 결과 메시지 작성
 	    PrintWriter out = resp.getWriter();
 	    MemberDAO mdao = new MemberDAO();
 	    MemberDTO member = mdao.login(userid, password);
 	    
+	    JSONObject result = new JSONObject();
 	    if(member == null) {
+	    	result.put("result", "fail");
 	    	System.out.println("로그인 실패");
-	    	out.print("{\"result\" : \" 실패 \" }");
+	    	out.print(result.toJSONString());
 	    	return super.excute(req, resp);
 	    }
-	    System.out.println("유저 ID : " + userid);
-	    System.out.println("비밀번호 : " + password);
 	    
 	    // (임시)세센에 로그인 정보 저장
 	    HttpSession session = req.getSession();
 	    session.setAttribute("member", member);
 	    
 	    System.out.println(session.getAttribute("member").toString());
-	    out.print("{\"result\" : \"성공\" }");
+	    result.put("result", "success");
+	    result.put("url", req.getContextPath());
+	    out.print(result.toJSONString());
 	    
 		return super.excute(req, resp);
 	}
