870aa063fe5e16339dedc1edd8d9725b6781be07
jeonbyungho <byho.jeon@gmail.com>
2023-09-06 13:57:42 +0900

    Revert "로그아웃 임시 구현"
    This reverts commit 4aa7405a2d9a965978b4b80745b6b31c4fcf6232.

--- src/main/java/com/web/action/MemberLogoutAction.java
+++ src/main/java/com/web/action/MemberLogoutAction.java
@@ -12,12 +12,12 @@ public class MemberLogoutAction extends ExcuteAction{
 		HttpSession session = req.getSession();
 		String userid = (String) session.getAttribute("userid");
 		
-		// 로그인 여부 검사
 		if(userid != null) {
 			session.removeAttribute("userid");
-			session.setAttribute("logout", "success");
+			System.out.println(userid + " 로그아웃!");
+			
 			setRedirect(true);
-			setPath(req.getContextPath() +"/");
+			setPath( req.getContextPath() +"/");
 		} else {
 			System.out.println("Error:로그인되지 않는 상태로 로그아웃 시도!");
 		}
