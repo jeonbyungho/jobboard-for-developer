53615ac6e427640c5f7c2e8ac0686fb510cea477
jeonbyungho <byho.jeon@gmail.com>
2023-09-14 12:42:55 +0900

    구인글 작성 post 데이터 오류 해결

--- src/main/java/com/web/action/ArticleInsertAction.java
+++ src/main/java/com/web/action/ArticleInsertAction.java
@@ -1,12 +1,18 @@
 package com.web.action;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
 
 import com.dto.ArticleDTO;
 import com.dto.BranchDAO;
+import com.dto.CompanyDTO;
+import com.dto.UserDTO;
 
 public class ArticleInsertAction extends ExcuteAction{
 	@Override
@@ -15,7 +21,15 @@ public class ArticleInsertAction extends ExcuteAction{
 		BranchDAO bdao = new BranchDAO();
 		ArticleDTO article = new ArticleDTO();
 		
-		article.setCompany_id(1);
+		HttpSession session = req.getSession();
+		CompanyDTO company = (CompanyDTO) session.getAttribute("member");
+		System.out.println(company.toString());
+		
+		PrintWriter out = resp.getWriter();
+		JSONObject result = new JSONObject();
+		
+		// 파라미터 데이터 구성
+		article.setCompany_id(company.getId());
 		article.setTitle(req.getParameter("title"));
 		article.setSalary(Integer.parseInt(req.getParameter("salary")));
 		article.setAddress(req.getParameter("address"));
@@ -25,30 +39,28 @@ public class ArticleInsertAction extends ExcuteAction{
 		article.setDeadline(req.getParameter("deadline"));
 		article.setQualify(req.getParameter("qualify"));
 		
+		// 경력
 		String[] corrArr = req.getParameterValues("career");
 		String newcorrArr = String.join(", ", corrArr);
 		article.setCareer(newcorrArr);
 		
-		
+		// 직무
 		String[] job = req.getParameterValues("job");
 		article.setJob(job);
 		
+		// 기술 스택
 		String[] skill_stack = req.getParameterValues("stack");
 		article.setSkill_stack(skill_stack);
 		System.out.println(article.toString());
 		
-		boolean result = bdao.article_insert(article);
-		// dao
-		System.out.println("Insert 성공 여부 : " + result);
+		// 구인글 작성
+		boolean result1 = bdao.article_insert(article);
+		System.out.println("Insert 성공 여부 : " + result1);
 		
-		setRedirect(true);
-		
-		if(bdao.article_insert(article)) {
-			int idx = bdao.article_idmax();
-			setPath("./article/"+idx);
-		} else {
-			setPath("./article");
-		}
+		result.put("result", "success");
+		int idx = bdao.article_idmax();
+		result.put("url", req.getContextPath()+"/article/"+ idx);
+		out.print(result.toJSONString());
 		
 		return super.excute(req, resp);
 		
--- src/main/webapp/resource/js/article-write.js
+++ src/main/webapp/resource/js/article-write.js
@@ -1,66 +1,42 @@
 console.log("👌article-write");
-
-//document.myForm.onsubmit = (e) => {
-//	e.preventDefault();
-//	const form = e.target;
-//	let formdata = new FormData();
-//	
-//	formdata.append('job',['모바일', '프론트엔드', '풀스택', '백엔드', '데이터', '언리얼엔진', 'AI', '기타'])
-//	formdata.get('job');
-//	
-//	formdata.append('carr', ['신입', '경력', '인턴']);
-//	formdata.get('carr');
-//	
-//	formdata.append('stack', ['Java', 'Spring Boot', 'Spring', 'Android', 'Unreal5', 'MySQL', 'PHP', 'AJAX', 'MSSQL', 'JSP', 'Flutter', 'Python', 'R', 'Pytorch', 'Tensorflow']);
-//	formdata.get('stack');
-//	
-//	let values = formdata.values();
-//	for (const pair of values) {
-//	    console.log(pair); 
-//}
-//	
-//	fetch('https://localhost/article/', {
-//    method: 'POST',
-//    cache: 'no-cache',
-//    body: formdata // body 부분에 폼데이터 변수를 할당
-//})
-//};
-
-	async function article_wirte(e) {
+document.myForm.onsubmit = async function article_wirte(e) {
 	e.preventDefault();
 	
 	// form 데이터 구성
 	const form = e.target;
 	
-	let formData = new FormData(form);
-	
 	const address = form.address.value;
-	document.writeln(address);
-	const addr1 = address.split(' ')[0];
-	document.writeln(addr1);
+	const addr1 = address.split(' ', 1).join();
 	const addr2 = address.substring(addr1.length);
-	const job1 = formData.append('job',['모바일', '프론트엔드', '풀스택', '백엔드', '데이터', '언리얼엔진', 'AI', '기타']);
 	
-	
-	
-	const carr1 = formData.append('carr', ['신입', '경력', '인턴']);
-	const stack1 = formData.append('stack', ['Java', 'Spring Boot', 'Spring', 'Android', 'Unreal5', 'MySQL', 'PHP', 'AJAX', 'MSSQL', 'JSP', 'Flutter', 'Python', 'R', 'Pytorch', 'Tensorflow']);
+	const career = []
+	form.career.forEach((e)=> {
+		if(e.checked) career.push(e.value)
+	});
+	const job = []
+	form.job.forEach((e)=> {
+		if(e.checked) job.push(e.value)
+	});
+	const stack = []
+	form.stack.forEach((e)=> {
+		if(e.checked) stack.push(e.value)
+	});
 	
 	const ArticleWriteData = {
 			title : form.title.value,
-			job : job1,
-			carr : carr1,
+			job : job,
+			stack : stack,
+			career : career,
 			salary : form.salary.value,
 			need_amt : form.need_amt.value,
 			deadline : form.deadline.value,
 			job_explain : form.job_explain.value,
 			qualify : form.qualify.value,
-			stack : stack1,
 			address : addr1,
 			detail_addr : (addr2 + form.detail_addr.value + form.addretc.value).trim(),
 		};
 	console.log(form.action);
-	console.table(loginData);
+	console.table(ArticleWriteData);
 	
 	// 회원가입 요청
 	try {
@@ -78,14 +54,14 @@ console.log("👌article-write");
 		
 		// 회원 가입 성공
 		if(jsonData.result == "success"){
-			alert('aaaa');
+			alert('작성 완료');
 			location = jsonData.url;
 		// 실패
-		} else if(jsonData.result == "fail") alert('회원가입을 실패하였습니다.');
+		} else if(jsonData.result == "fail") alert('작성 실패.');
 		
 	// 오류 발생
 	} catch (error) {
 		console.error("Error:", error);
-		alert('bbbb');
+		alert('알 수 없는 에러 발생!');
 	}
 }
